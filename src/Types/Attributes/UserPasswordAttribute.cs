63ea48d0b309b8cbb98ac031b9f1276fefb982e2
zawisz <zawistowski.zawisz@gmail.com>
2020-07-20 10:14:17 +0200

    Custom attribute

---
+++ src/Types/Attributes/UserPasswordAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Types.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class UserPasswordAttribute : ValidationAttribute
+    {
+        public int Length { get; }
+
+        public UserPasswordAttribute(int length = 4)
+        {
+            Length = length;
+        }
+
+        public override bool IsValid(object value)
+        {
+            var userPassword = (string)value;
+
+            if (userPassword.Length >= Length)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/Types/Interfaces/UsingIDisposable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Types.Interfaces
+{
+    public class UsingIDisposable
+    {
+        public class DatabaseConnection : IDisposable
+        {
+            public void Dispose()
+            {
+                Console.WriteLine("Dispose callad");
+            }
+
+            public static void Test()
+            {
+                using (DatabaseConnection conn = new DatabaseConnection())
+                {
+                    // do something with the db connection
+                }
+            }
+        }
+    }
+}
