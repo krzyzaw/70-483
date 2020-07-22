4e28cc84c7a1f6e592433ce0e572e0eb8f36e3eb
zawisz <zawistowski.zawisz@gmail.com>
2020-07-22 11:32:21 +0200

    Add MVC project + EF

---
+++ src/MusicStore/Data/MusicStoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MusicStore.Entities;
+
+namespace MusicStore.Data
+{
+    public class MusicStoreContext : DbContext
+    {
+        public MusicStoreContext (DbContextOptions<MusicStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MusicStore.Entities.MusicTrack> MusicTrack { get; set; }
+    }
+}
---
+++ src/MusicStore/Entities/MusicTrack.cs
@@ -0,0 +1,13 @@
+﻿namespace MusicStore.Entities
+{
+    public class MusicTrack
+    {
+        public int Id { get; set; }
+
+        public string Artist { get; set; }
+
+        public string Title { get; set; }
+
+        public int Length { get; set; }
+    }
+}
