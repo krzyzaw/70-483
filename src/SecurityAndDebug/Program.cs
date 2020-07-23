d287c0e38c0c3073046bf6b7b866f31d7861c37a
zawisz <zawistowski.zawisz@gmail.com>
2020-07-23 18:45:49 +0200

    Regex

--- src/SecurityAndDebug/Program.cs
+++ src/SecurityAndDebug/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SecurityAndDebug.ValidTypes;
 
 namespace SecurityAndDebug
 {
@@ -6,7 +7,8 @@ namespace SecurityAndDebug
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RegexExploration.RegexReplace();
+            Console.ReadKey();
         }
     }
 }
