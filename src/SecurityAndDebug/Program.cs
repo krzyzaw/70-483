aded7b2490a97f50b1a721abe124db5c6ebe9bd0
zawisz <zawistowski.zawisz@gmail.com>
2020-07-24 08:10:51 +0200

    Listener

--- src/SecurityAndDebug/Program.cs
+++ src/SecurityAndDebug/Program.cs
@@ -7,19 +7,12 @@ namespace SecurityAndDebug
     {
         static void Main(string[] args)
         {
-            Trace.WriteLine("Starting the program");
-            Trace.Indent();
-            Trace.WriteLine("Inside a function");
-            Trace.Indent();
-            Trace.WriteLine("Outside a function");
+            TraceListener consoleListener = new ConsoleTraceListener();
+            Trace.Listeners.Add(consoleListener);
 
-            var iUnitQty = 100;
-            Trace.WriteLineIf(iUnitQty > 50, "This message WILL appear");
-            Trace.WriteLineIf(iUnitQty < 50, "This message will NOT appear");
-
-            double dUnitCost = 1.03;
-            Trace.Assert(dUnitCost > 1, "Message will NOT appear");
-            Trace.Assert(dUnitCost < 1, "Message will appear since dUnitcost < 1 is false");
+            Trace.TraceInformation("Info message");
+            Trace.TraceWarning("Warning message");
+            Trace.TraceError("ErrorMessage");
 
             Console.ReadKey();
         }
