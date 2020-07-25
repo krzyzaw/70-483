dfb7c4d378698b65a6d1c48c17a298de3e5edba9
zawisz <zawistowski.zawisz@gmail.com>
2020-07-25 18:54:16 +0200

    out params

---
+++ src/ManageFlow/Methods/TestMethod.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+
+namespace ManageFlow.Methods
+{
+    public class TestMethod
+    {
+        public static int Add(int x, int y)
+            => x + y;
+
+        public static void Add(int x, int y, out int ans)
+            => ans = x + y;
+
+        public static void ReturnManyValue(out int a, out string b, out bool c)
+        {
+            a = 9;
+            b = "Enjoy your string.";
+            c = true;
+        }
+    }
+}
--- src/ManageFlow/Program.cs
+++ src/ManageFlow/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using ManageFlow.Methods;
 
 namespace ManageFlow
 {
@@ -6,8 +8,14 @@ namespace ManageFlow
     {
         static void Main(string[] args)
         {
-            Loops.LoopsExploration.TestBrakeStatement();
-            Loops.LoopsExploration.TestContinueStatement();
+            TestMethod.Add(1, 2, out int ans);
+            Console.WriteLine(ans);
+
+            TestMethod.ReturnManyValue(out var i, out var str, out var b);
+            Console.WriteLine("Int is: {0}", i);
+            Console.WriteLine("String is: {0}", str);
+            Console.WriteLine("Boolean is: {0}", b);
+            
             Console.ReadKey();
         }
     }
