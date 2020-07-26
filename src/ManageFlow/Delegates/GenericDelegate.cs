f265fa7e6deb45c8bccd94cae64cdf0fe066fb5a
zawisz <zawistowski.zawisz@gmail.com>
2020-07-26 10:30:12 +0200

    generic delegates

---
+++ src/ManageFlow/Delegates/GenericDelegate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+namespace ManageFlow.Delegates
+{
+    public class GenericDelegate
+    {
+        public delegate void Write<T>(T message);
+
+        public static void TestGenericDelegate()
+        {
+            Write<string> strTarget = new Write<string>(Writer);
+            Writer("test");
+
+            CheckTemperature(TooHighAlert, TempIsOptimal);
+        }
+
+        public static void Writer(string arg)
+        {
+            Console.WriteLine($"{arg}");
+        }
+
+        public static void CheckTemperature(Write<string> optimal, Write<string> tooHigh)
+        {
+            var temperature = 50;
+            var random = new Random();
+
+            while (true)
+            {
+                var change = random.Next(-5, 5);
+                temperature += change;
+
+                if (temperature <= 50)
+                {
+                    optimal("Optimal");
+                }
+                else
+                {
+                    tooHigh("Too high");
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+
+        public static void TooHighAlert(string alertMessage)
+        {
+            Console.WriteLine(alertMessage);
+        }
+
+        public static void TempIsOptimal(string optimalMessage)
+        {
+            Console.WriteLine(optimalMessage);
+        }
+    }
+}
--- src/ManageFlow/Program.cs
+++ src/ManageFlow/Program.cs
@@ -8,24 +8,7 @@ namespace ManageFlow
     {
         static void Main(string[] args)
         {
-            Car car = new Car(brand:"Ford",maxSpeed: 100, currentSpeed: 10);
-            var handler = new Car.CarEngineHandler(Car.OnCarEngineEvent);
-            var handler2 = new Car.CarEngineHandler(Car.OnCarEngineEvent2);
-
-            car.RegisterWithCarEngine(handler);
-            car.RegisterWithCarEngine(handler2);
-
-            Console.WriteLine("***** Speeding up *****");
-
-            // Zwiększ prędkość (w ten sposób zostaną wyzwolone zdarzenia).
-            for (int i = 0; i < 6; i++)
-                car.Accelerate(20);
-
-            car.UnRegisterWithCarEngine(handler2);
-
-            for (int i = 0; i < 6; i++)
-                car.Accelerate(20);
-
+            GenericDelegate.TestGenericDelegate();
             Console.ReadLine();
         }
     }
