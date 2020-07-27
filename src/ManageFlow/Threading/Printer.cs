d7dc609d8c0aefd05f9626cb72e0ad8cfadc3c77
zawisz <zawistowski.zawisz@gmail.com>
2020-07-27 09:26:33 +0200

    monitor

--- src/ManageFlow/Threading/Printer.cs
+++ src/ManageFlow/Threading/Printer.cs
@@ -9,23 +9,26 @@ namespace ManageFlow.Threading
 
         public void PrintNumbers()
         {
-            lock (threadLock)
+            Monitor.Enter(threadLock);
+            try
             {
                 // Wyświetl informacje o wątku.
-                Console.WriteLine("-> {0} is executing PrintNumbers()",
-                    Thread.CurrentThread.Name);
+                Console.WriteLine("-> {0} is executing PrintNumbers()", Thread.CurrentThread.Name);
 
                 // Wyświetl liczby.
                 Console.Write("Your numbers: ");
                 for (int i = 0; i < 10; i++)
                 {
-                    // Uśpij wątek na losowy czas.
                     Random r = new Random();
                     Thread.Sleep(1000 * r.Next(5));
                     Console.Write("{0}, ", i);
                 }
 
                 Console.WriteLine();
+            }
+            finally
+            {
+                Monitor.Exit(threadLock);
             }
         }
     }
