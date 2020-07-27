c4b2d9a78c740f99923f2ca8ab2d4c6f25df9e0a
zawisz <zawistowski.zawisz@gmail.com>
2020-07-27 09:53:33 +0200

    task

---
+++ src/ManageFlow/Threading/TaskExploration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ManageFlow.Threading
+{
+    public class TaskExploration
+    {
+        public static void Test()
+        {
+            // tworzenie nowego wątku
+            var thread = new Thread(start =>
+            {
+                // zadanie do wykonania
+            });
+            thread.Start();
+
+            // tworzenie nowego zadania
+            Task.Factory.StartNew(() =>
+            {
+                // zadanie do wykonania
+            });
+        }
+    }
+}
