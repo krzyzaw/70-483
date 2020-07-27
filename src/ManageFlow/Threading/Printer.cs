022f064b944baa382245dead33128bd7fcf47342
zawisz <zawistowski.zawisz@gmail.com>
2020-07-27 09:26:08 +0200

    lock

--- src/ManageFlow/Threading/Printer.cs
+++ src/ManageFlow/Threading/Printer.cs
@@ -5,23 +5,28 @@ namespace ManageFlow.Threading
 {
     public class Printer
     {
+        private object threadLock = new object();
+
         public void PrintNumbers()
         {
-            // Wyświetl informacje o wątku.
-            Console.WriteLine("-> {0} is executing PrintNumbers()",
-                Thread.CurrentThread.Name);
-
-            // Wyświetl liczby.
-            Console.Write("Your numbers: ");
-            for (int i = 0; i < 10; i++)
+            lock (threadLock)
             {
-                // Uśpij wątek na losowy czas.
-                Random r = new Random();
-                Thread.Sleep(1000 * r.Next(5));
-                Console.Write("{0}, ", i);
-            }
+                // Wyświetl informacje o wątku.
+                Console.WriteLine("-> {0} is executing PrintNumbers()",
+                    Thread.CurrentThread.Name);
 
-            Console.WriteLine();
+                // Wyświetl liczby.
+                Console.Write("Your numbers: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    // Uśpij wątek na losowy czas.
+                    Random r = new Random();
+                    Thread.Sleep(1000 * r.Next(5));
+                    Console.Write("{0}, ", i);
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
